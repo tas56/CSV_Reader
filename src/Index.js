8357d25f55cdfea30d2f7f5669919ef969865546
Thomas Semiz <tas56@njit.edu>
2021-02-24 19:42:58 -0500

    Created ReadStream class

--- src/Index.js
+++ src/Index.js
@@ -1,28 +1,77 @@
 const parse = require('csv-parse')
 const fs = require('fs');
 const City = require('models/City')
-const output = []
 let csvFile = '../data/worldcities.csv';
+const output = []
+
+class ReadStream {
+
+    static output = [];
+
+    constructor(csvFile,model) {
+
+        this.csvFile = csvFile;
+        this.model = model;
+
+    }
+
+    static Create(csvFile,model){
+        return new ReadStream(csvFile,model);
+    }
+
+    static AddOutput(record){
+        this.output.push(record);
+    }
+
+    CreateReadStream(){
+        fs.createReadStream(this.csvFile)
+            .pipe(parse({
+            columns: true,
+            delimiter: ',',
+            trim: true,
+            skip_empty_lines: true
+        })
+            .on('readable', function(){
+                let record
+                while (record = this.read()) {
+                    console.log(record)
+                    let city = this.model.create(record);
+                    this.AddOutput(record)
+                }
+            })
+            // When we are done, test that the parsed output matched what expected
+            .on('end', function(){
+
+              //  console.log(output);
+
+            }));
+
+    }
 
 
-fs.createReadStream(csvFile)
-    .pipe(parse({
-    columns: true,
-    delimiter: ',',
-    trim: true,
-    skip_empty_lines: true
-})
-    .on('readable', function(){
-        let record
-        while (record = this.read()) {
-            console.log(record)
-            let city = City.create(record);
-            output.push(record)
-        }
-    })
-    // When we are done, test that the parsed output matched what expected
-    .on('end', function(){
+}
 
-      //  console.log(output);
 
-    }));
+
+
+// fs.createReadStream(csvFile)
+//     .pipe(parse({
+//     columns: true,
+//     delimiter: ',',
+//     trim: true,
+//     skip_empty_lines: true
+// })
+//     .on('readable', function(){
+//         let record
+//         while (record = this.read()) {
+//             console.log(record)
+//             let city = City.create(record);
+//             output.push(record)
+//         }
+//     })
+//     // When we are done, test that the parsed output matched what expected
+//     .on('end', function(){
+//
+//       //  console.log(output);
+//
+//     }));
